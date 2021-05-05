ddb1c4a36351b4be279cb64e5a524a92e2ad1427
vishwas-sharma <vishwastundla@gmail.com>
2021-05-06 00:02:18 +0530

    optimized code of Fibonacci series

--- Q9/test.js
+++ Q9/test.js
@@ -7,29 +7,50 @@ function fibonacciSeries(){
     document.getElementById("container").innerHTML=template; 
     document.getElementById("btn").addEventListener("click",()=>{
         let num=parseInt(document.getElementById("num").value);
-        console.log(num);
+        
+       // if(isNaN(num)){
+         //   document.getElementById("result").innerHTML=`Please enter valid number of terms of fibonacci series`;
+
+       // }else{
+            //let x=0,y=1,nextTerm=null;
+            // for(let i=1;i<=num;i++){
+            //     if(i==1){
+            //         document.getElementById("result").innerHTML=x;  
+            //     }else if(i==2){
+            //         document.getElementById("result").innerHTML+=`  ${y}`;
+            //     }else{
+            //         nextTerm=x+y;
+            //         x=y;
+            //         y=nextTerm;
+            //         document.getElementById("result").innerHTML+=`  ${nextTerm}`;
+
+            //     }
+            // }
+            
+       // }
+
+
+       
+       // program to generate fibonacci series up to n terms
         if(isNaN(num)){
             document.getElementById("result").innerHTML=`Please enter valid number of terms of fibonacci series`;
 
         }else{
-            let x=0,y=1,nextTerm=null;
-            for(i=1;i<=num;i++){
-                if(i==1){
-                    document.getElementById("result").innerHTML=x;  
-                }else if(i==2){
-                    document.getElementById("result").innerHTML+=`  ${y}`;
-                }else{
-                    nextTerm=x+y;
-                    x=y;
-                    y=nextTerm;
-                    document.getElementById("result").innerHTML+=`  ${nextTerm}`;
-
-                }
-            }
-
+       let x=0,y=1,nextTerm=null;
+       for (let i = 1; i <= num; i++) {
+        document.getElementById("result").innerHTML+=` ${x}`;              
+        //console.log(x);
+        nextTerm = x + y;
+        x = y;
+        y = nextTerm;
+          }
         }
-        
         
     })
 }
 fibonacciSeries();
+            
+            
+
+              
+
